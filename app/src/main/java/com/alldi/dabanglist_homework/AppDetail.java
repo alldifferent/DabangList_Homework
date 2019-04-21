f2de4b16eb3e992a1331bcee66e7b860a294e1cb
alldifferent <alldifferent@naver.com>
2019-04-22 00:09:30 +0900

    이미지 삽입

--- app/src/main/java/com/alldi/dabanglist_homework/AppDetail.java
+++ app/src/main/java/com/alldi/dabanglist_homework/AppDetail.java
@@ -1,13 +1,38 @@
 package com.alldi.dabanglist_homework;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alldi.dabanglist_homework.databinding.ActivityAppDetailBinding;
+import com.alldi.dabanglist_homework.datas.Dabang;
+
+import java.util.List;
+
 public class AppDetail extends AppCompatActivity {
+
+    ActivityAppDetailBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_detail);
+        act = DataBindingUtil.setContentView(this, R.layout.activity_app_detail);
+
+        setupEvents();
+        setValues();
+
+    }
+
+    void setupEvents(){
+
+    }
+    void setValues(){
+
+        Dabang dabangData;
+        dabangData = (Dabang) getIntent().getSerializableExtra("앱정보");
+
+        act.paymentAndMTxt.setText(String.format("월세 %d/%d",dabangData.payment,dabangData.menthpayment));
+        act.optionContextTxt.setText(dabangData.optionroom);
+
     }
 }
